db84a19d9652dbf51d5c489dede206f3399fc79e
mustafkrca <mustafkrca@gmail.com>
2024-06-26 17:23:17 +0300

    statePrices, cityPrices, navbar

--- client/main.js
+++ client/main.js
@@ -1,13 +1,20 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import '../imports/ui/fuelPrices/fuelPrices.html'
-import '../imports/ui/fuelPrices/fuelPrices.css'
-import '../imports/ui/fuelPrices/fuelPrices.js'
+import '../imports/ui/statePrices/statePrices.html'
+import '../imports/ui/statePrices/statePrices.css'
+import '../imports/ui/statePrices/statePrices.js'
+
+import '../imports/ui/cityPrices/cityPrices.js'
+import '../imports/ui/cityPrices/cityPrices.css'
+import '../imports/ui/cityPrices/cityPrices.html'
+
 
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './main.html';
+
+
 
 
--- imports/ui/cityPrices/cityPrices.js
+++ imports/ui/cityPrices/cityPrices.js
@@ -1,12 +1,12 @@
-// imports/ui/fuelPrices.js
+// imports/ui/statePrices.js
 
 import { Template } from 'meteor/templating';
-import './fuelPrices.html';
-import './fuelPrices.css';
+import './cityPrices.html';
+import './cityPrices.css';
 
-Template.fuelPrices.helpers({
+Template.cityPrices.helpers({
   cities: [
-    { city: 'İstanbul', regular: '7.50', midgrade: '8.20', premium: '8.80', diesel: '6.50' },
+    { city: 'New York', regular: '7.50', midgrade: '8.20', premium: '8.80', diesel: '6.50' },
     { city: 'Ankara', regular: '7.40', midgrade: '8.10', premium: '8.70', diesel: '6.40' },
     { city: 'İzmir', regular: '7.60', midgrade: '8.30', premium: '8.90', diesel: '6.60' },
     // Buraya istediğiniz şehirleri ve fiyatları ekleyebilirsiniz
