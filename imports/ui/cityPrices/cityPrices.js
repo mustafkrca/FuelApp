ccc88a401b545d9b55483842d369726114440da3
mustafkrca <mustafkrca@gmail.com>
2024-06-27 10:22:19 +0300

    statePrices veri gösterimi

--- imports/ui/cityPrices/cityPrices.js
+++ imports/ui/cityPrices/cityPrices.js
@@ -1,14 +1,24 @@
-// imports/ui/statePrices.js
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+// import './cityPrices.html';
+// import './cityPrices.css';
 
-import { Template } from 'meteor/templating';
-import './cityPrices.html';
-import './cityPrices.css';
+// Template.cityPrices.onCreated(function() {
+//   this.cityPrices = new ReactiveVar([]);
 
-Template.cityPrices.helpers({
-  cities: [
-    { city: 'New York', regular: '7.50', midgrade: '8.20', premium: '8.80', diesel: '6.50' },
-    { city: 'Ankara', regular: '7.40', midgrade: '8.10', premium: '8.70', diesel: '6.40' },
-    { city: 'İzmir', regular: '7.60', midgrade: '8.30', premium: '8.90', diesel: '6.60' },
-    // Buraya istediğiniz şehirleri ve fiyatları ekleyebilirsiniz
-  ]
-});
+//   const state = this.data.state; // state parametresini alın
+
+//   Meteor.call('cityPricesByState', state, (error, result) => {
+//     if (error) {
+//       console.error('API çağrısı başarısız:', error);
+//     } else {
+//       this.cityPrices.set(result);
+//     }
+//   });
+// });
+
+// Template.cityPrices.helpers({
+//   cities() {
+//     return Template.instance().cityPrices.get();
+//   }
+// });
--- imports/ui/statePrices/statePrices.js
+++ imports/ui/statePrices/statePrices.js
@@ -1,14 +1,45 @@
-// imports/ui/statePrices.js
-
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 import './statePrices.html';
 import './statePrices.css';
+import { HTTP } from 'meteor/http'; // HTTP paketini ekledik
+
+Template.statePrices.onCreated(function () {
+  this.statePrices = new ReactiveVar([]);
+
+  const options = {
+    method: 'GET',
+    hostname: 'api.collectapi.com',
+    path: '/gasPrice/allUsaPrice',
+    headers: {
+      'content-type': 'application/json',
+      authorization: 'apikey 2Z5VTj6hIzrnDAirco0p7J:4clq6oJ7yhjO2cCc0OqgZh', // API anahtarını buraya ekleyin
+    },
+  };
+
+  HTTP.call('GET', 'https://api.collectapi.com/gasPrice/allUsaPrice', options, (error, result) => {
+    if (error) {
+      console.error('API çağrısı başarısız:', error);
+    } else {
+      console.log('result.content', result.content)
+      this.statePrices.set(JSON.parse(result.content));
+    }
+  });
+});
 
 Template.statePrices.helpers({
-  cities: [
-    { city: 'İstanbul', regular: '7.50', midgrade: '8.20', premium: '8.80', diesel: '6.50' },
-    { city: 'Ankara', regular: '7.40', midgrade: '8.10', premium: '8.70', diesel: '6.40' },
-    { city: 'İzmir', regular: '7.60', midgrade: '8.30', premium: '8.90', diesel: '6.60' },
-    // Buraya istediğiniz şehirleri ve fiyatları ekleyebilirsiniz
-  ]
+  cities() {
+    const statePrices = Template.instance().statePrices.get();
+    if (statePrices && statePrices.result) {
+      return statePrices.result.map(city => ({
+        city: city.name,  
+        regular: city.gasoline,
+        midgrade: city.midGrade,
+        premium: city.premium,
+        diesel: city.diesel
+      }));
+    } else {
+      return []; // Veri yoksa boş dizi döndür
+    }
+  },
 });
