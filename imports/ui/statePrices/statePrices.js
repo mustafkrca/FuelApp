18c825664c59e3652a46a51dbf25ef399b474a40
mustafkrca <mustafkrca@gmail.com>
2024-07-01 15:22:37 +0300

    state code eklendi

--- imports/ui/statePrices/statePrices.js
+++ imports/ui/statePrices/statePrices.js
@@ -1,43 +1,41 @@
-// imports/ui/statePrices/statePrices.js
-
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { setStates,getStates } from '../../lib/datas.js'; // Veri getirmek için
-
-import '../navbarSlider/navbarSlider.js';
+import { setStates, getStates, setStateCodes, getStateCodes } from '../../lib/datas.js';
 import './statePrices.html';
 import './statePrices.css';
-import { fetchStatePrices } from '../../api/fuelPrices.js';
+import { fetchStatePrices, fetchStateCodes } from '../../api/fuelPrices.js';
 
 Template.statePrices.onCreated(function () {
-
-  fetchStatePrices()
-    .then(data => {
-      setStates(data.result); // Sadece sonuç dizisini ayarla
-
+  // Fetch state prices and codes
+  Promise.all([fetchStatePrices(), fetchStateCodes()])
+    .then(([statePricesData, stateCodesData]) => {
+      setStates(statePricesData.result);
+      setStateCodes(stateCodesData); // Assuming stateCodesData is an array
     })
     .catch(error => {
-      console.error('API çağrısı başarısız:', error);
+      console.error('API calls failed:', error);
     });
-
-  
 });
 
 Template.statePrices.helpers({
   states() {
     const statePrices = getStates();
-    if (statePrices) {
-      // Burada veri getirmek için kullanılan kodu güncelliyoruz
-      return statePrices.map(state => ({
+    const stateCodes = getStateCodes();
+    const newStateCodes = statePrices.map(state => {
+      const stateCodeObj = stateCodes.find(code => code.name === state.name);
+      return {
         state: state.name,
+        stateCode: stateCodeObj ? stateCodeObj.code : '', // Get corresponding state code or empty string if not found
         regular: state.gasoline,
         midgrade: state.midGrade,
         premium: state.premium,
         diesel: state.diesel
-      }));
-    } 
-    else {
-      return []; // Veri yoksa boş dizi döndür
+      };
+    });
+
+    if (statePrices && stateCodes) {
+      return newStateCodes;
+    } else {
+      return []; // Return empty array if data is not available
     }
-  },
+  }
 });
